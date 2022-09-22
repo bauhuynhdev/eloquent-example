6b0eed4b436a4d4ac9da8e051b735c92c8c0b954
Bau Huynh <bauhuynhdev@gmail.com>
2022-09-22 11:42:47 +0700

    - Update

---
+++ mongo.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+$capsule->getDatabaseManager()->extend(
+    'mongodb',
+    function ($config, $name) {
+        $config['name'] = $name;
+        return new Jenssegers\Mongodb\Connection($config);
+    }
+);
+$capsule->addConnection(
+    [
+        'driver' => 'mongodb',
+        'host' => '192.168.72.128',
+        'database' => 'bau',
+        'username' => 'root',
+        'password' => 'none',
+        'options' => [
+            'authSource' => 'admin',
+            'replicaSet' => 'rs0',
+            'directConnection' => true
+        ]
+    ]
+);
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$time = 0;
+Capsule::listen(
+    function ($query) use (&$time) {
+//        var_dump($query->sql);
+//        var_dump($query->time);
+        $time += $query->time;
+    }
+);
+
+class User extends Jenssegers\Mongodb\Eloquent\Model
+{
+    public $timestamps = false;
+    protected $primaryKey = 'id';
+    protected $collection = 'users';
+    protected $fillable = ['name', 'email'];
+}
+
+$faker = Faker\Factory::create();
+for ($i = 1; $i <= 1000000; $i++) {
+    User::create(
+        [
+            'email' => $faker->email(),
+            'name' => $faker->name()
+        ]
+    );
+    var_dump('Created: ' . $i);
+}
+
+var_dump('Total time: ' . $time);
---
+++ postgres.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+$capsule->addConnection(
+    [
+        'driver' => 'pgsql',
+        'host' => '192.168.72.128',
+        'database' => 'bau',
+        'username' => 'root',
+        'password' => 'none',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => '',
+    ]
+);
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$time = 0;
+Capsule::listen(
+    function ($query) use (&$time) {
+//        var_dump($query->sql);
+//        var_dump($query->time);
+        $time += $query->time;
+    }
+);
+
+class User extends Illuminate\Database\Eloquent\Model
+{
+    public $timestamps = false;
+    protected $table = 'users';
+    protected $primaryKey = 'email';
+    protected $fillable = ['name', 'email'];
+}
+
+$faker = Faker\Factory::create();
+for ($i = 1; $i <= 1000000; $i++) {
+    User::create(
+        [
+            'email' => $faker->email(),
+            'name' => $faker->name()
+        ]
+    );
+    var_dump('Created: ' . $i);
+}
+
+var_dump('Total time: ' . $time);
